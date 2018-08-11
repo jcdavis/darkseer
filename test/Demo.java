7cc3a0779a3e9e732c6c09b0e96aa866ad7fde06
Jackson Davis <jackson@jcdav.is>
2018-08-11 20:05:42 +0000

    Add comment in Demo about limitations

--- test/Demo.java
+++ test/Demo.java
@@ -1,5 +1,9 @@
 import is.jcdav.darkseer.DarkSeer;
 
+/* A Demo that shows the change in allocations as a return of string conat
+ * optimization in c2. Note that this is no longer interesting as of java 9+,
+ * since indy concat in means that the conccat is optimal immediately :)
+ */
 public class Demo {
   public static String test(String s1, String s2) {
     return s1 + s2;
